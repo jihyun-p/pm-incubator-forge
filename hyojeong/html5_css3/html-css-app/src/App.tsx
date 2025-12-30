6c2bf03628ef45e0c0a5309aee70e109de326447
hyojeonglee673-dot <hyojeonglee673@gmail.com>
2025-12-30 13:38:39 +0900

    feat: [이효정] Math 기능 및 switch 키워드 활용 [PMIF-x]

--- hyojeong/html5_css3/html-css-app/src/App.tsx
+++ hyojeong/html5_css3/html-css-app/src/App.tsx
@@ -2,15 +2,16 @@ import './App.css'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
 import { VariableAndDataTypes } from './data_type/VariableAndDataTypes'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
-import { CalcuationExample } from './math_operation/CalculationExample'
+import { CalculationExample } from './math_operation/CalculationExample'
 import { FirstProblem } from './problem/FirstProblem'
+
 
 function App() {
 
   return (
   
     <div>
-      <CalcuationExample></CalcuationExample>
+      <CalculationExample></CalculationExample>
       <VariableAndDataTypes></VariableAndDataTypes>
       <LogicalOperationExample></LogicalOperationExample>
       <ControlFlowIf></ControlFlowIf>
